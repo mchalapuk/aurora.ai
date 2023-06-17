e3131ab52e62bc960c3f1d672865d12acda1d946
Maciej Chałapuk <maciej@chalapuk.pl>
2023-06-17 23:15:20 +0200

    + loading llama model and qdrant vectorstore

--- aurora/main.py
+++ aurora/main.py
@@ -1,6 +1,24 @@
+import logging
+from typing import Optional
+
 from fastapi import FastAPI, Request
 
+from langchain.embeddings import LlamaCppEmbeddings
+from langchain.vectorstores import VectorStore, Qdrant
+import qdrant_client
+
 app = FastAPI()
+embeddings = None
+vectorstore: Optional[VectorStore] = None
+
+@app.on_event("startup")
+async def startup_event():
+    logging.info("loading embeddings")
+    embeddings = LlamaCppEmbeddings(model_path="./model.bin")
+
+    logging.info("loading vectorstore")
+    client = qdrant_client.QdrantClient(path="./qdrant.db", prefer_grpc=True)
+    qdrant = Qdrant(client=client, collection_name="my_documents", embeddings=embeddings)
 
 @app.get("/")
 async def get(request: Request):
